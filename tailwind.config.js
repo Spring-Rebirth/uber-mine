4b33b3abfba2833a3175a1e9a574107d5eac1ae1
Spring-Rebirth <cjunwei6249@gmail.com>
2024-10-12 01:04:32 +0800

    init

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+const { content } = require('./tailwind.config');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
