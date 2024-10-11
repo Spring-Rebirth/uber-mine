2a21996693d1463af2af9ce6f8fd81fa6f267693
Spring-Rebirth <cjunwei6249@gmail.com>
2024-10-12 01:11:30 +0800

    整理文件

---
+++ app/index.jsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      className='w-screen h-screen justify-center items-center'
+    >
+      <Text className='text-blue-500 text-2xl'>
+        Edit app
+      </Text>
+    </View>
+  );
+}
